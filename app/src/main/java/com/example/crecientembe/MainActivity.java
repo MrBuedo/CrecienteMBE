bda1d79c5a17f85df17c5b891bf3454e7651c4c0
mrbue <mrbuedo@gmail.com>
2024-03-02 18:05:36 +0100

    lets go

---
+++ app/src/main/java/com/example/crecientembe/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.crecientembe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
